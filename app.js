4251612eeeaeb0538c4eead190e671bbc09e106d
todorovicd <dt9669@moval.edu>
2019-06-27 15:14:26 -0500

    add root query, type relations, mongoose with mlab and mutations

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect("mongodb://damjan:damjan123@ds013216.mlab.com:13216/gql-dom");
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
   })
 );
 
